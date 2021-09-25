85b4dd97a175700c62a96be0142706b24931959e
Rhutvij Chiplunkar <rhutvij.chiplunkar@gmail.com>
2021-09-25 18:21:37 +0530

    files sorted

--- Number system conversions.js
+++
@@ -1,9 +0,0 @@
-// function that converts the decimal number to other number systems
-function NumberConversions(){
-    var x=document.getElementById('num').value;
-    var y=Number(x);
-    document.getElementById('a').innerHTML+=y.toString(2);
-    document.getElementById('b').innerHTML+=y.toString(8);
-    document.getElementById('c').innerHTML+=y.toString(10);
-    document.getElementById('d').innerHTML+=y.toString(16);    
-}
